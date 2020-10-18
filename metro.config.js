6b54d70cda3e7335c9354ea8253c9af408ee3da0
Sayeed Afridi <as.afridi563@gmail.com>
2020-10-18 06:01:10 +0600

    Metro config

---
+++ metro.config.js
@@ -0,0 +1,15 @@
+const { getDefaultConfig } = require('metro-config')
+
+module.exports = (async () => {
+  const defaultConfig = await getDefaultConfig()
+  const { assetExts } = defaultConfig.resolver
+  return {
+    transformer: {
+      assetPlugins: ['expo-asset/tools/hashAssetFiles'],
+    },
+    resolver: {
+      // Add bin to assetExts
+      assetExts: [...assetExts, 'bin', 'jpeg', 'png'],
+    },
+  }
+})()
