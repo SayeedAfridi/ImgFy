1954882f3a2356eb8f1b80447f828e8dbbc542a2
Sayeed Afridi <as.afridi563@gmail.com>
2022-10-25 01:55:34 +0600

    chore: upgrade all packages and expo sdk

--- src/utils/picimage.js
+++ src/utils/picimage.js
@@ -1,26 +1,32 @@
 import * as ImagePicker from 'expo-image-picker'
-import { Platform, ToastAndroid } from 'react-native'
+import { ToastAndroid } from 'react-native'
+
+import { manipulateAsync, SaveFormat } from 'expo-image-manipulator';
 
 const pickImage = async (setImage) => {
   try {
-    if (Platform.OS === 'ios') {
-      const { status } = await ImagePicker.requestCameraRollPermissionsAsync()
-      if (status !== 'granted') {
-        alert('Sorry, we need camera roll permissions to make this work!')
-        return
-      }
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync()
+    if (status !== 'granted') {
+      alert('Sorry, we need camera roll permissions to make this work!')
+      return
     }
     let result = await ImagePicker.launchCameraAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
       aspect: [4, 4],
       quality: 1,
+
     })
     if (!result.cancelled) {
+      const manRes = await manipulateAsync(
+        result.uri,
+        [{ resize: { height: 500, width: 500 } }],
+        { compress: 1, format: SaveFormat.JPEG },
+      );
       if (setImage) {
-        setImage(result.uri)
+        setImage(manRes.uri)
       }
-      return result.uri
+      return manRes.uri
     }
   } catch (error) {
     console.log(error)
